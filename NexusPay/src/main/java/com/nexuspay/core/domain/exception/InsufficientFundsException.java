77c7c38dcdbc6d87ff7f34a9df49699c533728e2
Juan S. <regiomaster@gmail.com>
2026-01-05 14:22:02 -0800

    Delete NexusPay/src directory

--- NexusPay/src/main/java/com/nexuspay/core/domain/exception/InsufficientFundsException.java
+++
@@ -1,8 +0,0 @@
-package com.nexuspay.core.domain.exception;
-
-// Excepción personalizada para lógica de negocio
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
